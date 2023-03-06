061a60caa08f54026f87a28158aaf8516f0933a2
Troi Williams <40696868+troiwill@users.noreply.github.com>
2023-03-06 15:17:04 -0500

    Stripped down to standalone Python package.

--- sense_aruco/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['sense_aruco'],
-    package_dir={'': 'src'}
-)
-
-setup(**d)
